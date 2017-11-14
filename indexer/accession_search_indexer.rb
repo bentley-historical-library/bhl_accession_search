6f08720745bec1a84483ddd83efec59ad73ffda8
djpillen@umich.edu <djpillen@umich.edu>
2017-11-14 10:53:46 -0500

    update accession_search indexer

--- indexer/accession_search_indexer.rb
+++ indexer/accession_search_indexer.rb
@@ -11,18 +11,20 @@ class CommonIndexer
           doc['collection_management_processing_status_u_ustr'] = record['record']['collection_management']['processing_status']
         end
 
+        if record['record']['user_defined']
+          doc['baseline_u_sbool'] = record['record']['user_defined']['boolean_2']
+        end
+
         classification_identifiers = []
-        if doc['enum_1_enum_s']
-          classification_identifiers.concat(doc['enum_1_enum_s'])
+        (1..3).each do |i|
+          if doc.include?("enum_#{i}_enum_s")
+            classification_identifiers.concat(doc["enum_#{i}_enum_s"])
+          end
         end
-        if doc['enum_2_enum_s']
-          classification_identifiers.concat(doc['enum_2_enum_s'])
+
+        if !classification_identifiers.empty?
+          doc['classification_identifiers_u_sstr'] = classification_identifiers
         end
-        if doc['enum_3_enum_s']
-          classification_identifiers.concat(doc['enum_3_enum_s'])
-        end
-        doc['classification_identifiers_u_sstr'] = classification_identifiers
-        doc['baseline_u_sbool'] = record['record']['user_defined']['boolean_2']
       end
     }
   end
