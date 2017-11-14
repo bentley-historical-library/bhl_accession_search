98d6fc80ffa021f276663cc1f5720b3a9a430e77
djpillen@umich.edu <djpillen@umich.edu>
2017-11-14 09:29:57 -0500

    update indexer classname

--- indexer/accession_search_indexer.rb
+++ indexer/accession_search_indexer.rb
@@ -1,5 +1,6 @@
-class CommonIndexer
-  add_indexer_initialize_hook do |indexer|
+class IndexerCommon
+
+  self.add_indexer_initialize_hook do |indexer|
     indexer.add_document_prepare_hook {|doc, record|
       if doc['primary_type'] == 'accession'
         sources = record['record']['linked_agents'].select{|link| link['role'] === 'source'}
@@ -25,4 +26,5 @@ class CommonIndexer
       end
     }
   end
+  
 end
